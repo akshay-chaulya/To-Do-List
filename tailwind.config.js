4a8f141701b076369c2ec71ce4ceaf56084eb071
Akshay Chaulya <akshaychaulya09@gmail.com>
2024-02-09 18:52:03 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#374151',
+        darker: '#1F2937',
+        darkest: '#111827',
+        Cgray: '#6B7280',
+        light: '#EEE',
+        Cpink: '#EC4899',
+        Cpurple: '#8B5Cf6',
+        crimson: 'crimson',
+      }
+    },
+  },
+  plugins: [],
+}
